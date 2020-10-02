f6d7c849307683587390dadf566babe31c1dec36
tholakelebusi <tholakelebusi@gmail.com>
2020-10-02 01:05:22 +0100

    4 files modified, 8 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { CardsComponent } from './cards/cards.component';
-
+import { FashionComponent } from './fashion/fashion.component';
+import { TravelComponent } from './travel/travel.component';
+import { SportComponent } from './sport/sport.component';
+import { FoodComponent } from './food/food.component';
 const routes: Routes = [
   { path: 'TABS', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: '', component:CardsComponent},
+  { path: 'fashion', component:FashionComponent},
+  { path: 'travel', component:TravelComponent },
+  { path: 'sport', component:SportComponent},
+  { path: 'food', component:FoodComponent},
 ];
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
+import { FashionComponent } from './fashion/fashion.component';
+import { TravelComponent } from './travel/travel.component';
+import { SportComponent } from './sport/sport.component';
+import { FoodComponent } from './food/food.component';
 
 @NgModule({
-  declarations: [AppComponent, CardsComponent],
+  declarations: [AppComponent, CardsComponent, FashionComponent, TravelComponent, SportComponent, FoodComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
